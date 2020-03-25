8eef91785bd031c607a4b73274a7d5746aa80a18
Nikita <marcus01hemingway@gmail.com>
2020-03-25 17:55:56 +0300

    dayz

--- server.js
+++ server.js
@@ -12,47 +12,152 @@ const userScheme = new Schema({
     password: String,
     email: String,
     name: String,
-    age: Number
+    age: Number,
+    gender: String,
+    userId: String
 });
 
 const User = mongoose.model("User", userScheme);
 app.use(cors())
 
-mongoose.connect("mongodb://localhost:27017/usersdb", { useNewUrlParser: true }, function(err){
-    if(err) return console.log(err);
-    app.listen(3000, function(){
+mongoose.connect("mongodb://localhost:27017/usersdb", {useNewUrlParser: true}, function (err) {
+    if (err) return console.log(err);
+    app.listen(3000, function () {
         console.log("Сервер ожидает подключения...");
     });
 });
 
 app.post("/api/users", jsonParser, function (req, res) {
 
-    if(!req.body) {
+    if (!req.body) {
         console.log('WARNN ', req.body);
 
         return res.send("no body");
     }
 
     const userLogin = req.body.userLogin;
-    const saltRounds = 10;
+    let saltRounds = bcrypt.genSaltSync(10);
     const userPassword = bcrypt.hashSync(req.body.userPassword, saltRounds);
     const userEmail = req.body.userEmail;
     const userName = req.body.userName;
     const userAge = req.body.userAge;
 
     const user = new User({
-        login:userLogin,
+        login: userLogin,
         password: userPassword,
         email: userEmail,
         name: userName,
-        age: userAge
+        age: userAge,
+        userId: saltRounds
     });
 
-    user.save(function(err){
-        if(err){
+    user.save(function (err) {
+        if (err) {
             console.log('ERRRRR ', err);
             return console.log(err);
         }
         res.send(user);
     });
+});
+
+
+app.put("/api/users", jsonParser, function (req, res) {
+
+    if (!req.body) return res.sendStatus(404);
+    const getLogin = req.body.userLogin;
+    const getPassword = req.body.userPassword;
+    User.findOne({login: getLogin}, function (err, user) {
+
+        if (err) return console.log(err);
+        if (user === null) {
+            console.log('user not found');
+            res.status(400).send('User is not exist');
+        } else {
+
+            if (bcrypt.hashSync(getPassword, user.userId) === user.password) {
+                console.log("совпадение? не думаю");
+                res.status(200).send('success')
+            } else {
+                console.log("совпадение это не падение сов");
+                res.status(401).send('Wrong password')
+            }
+        }
+    });
+
+});
+
+app.get("/api/users/:id", function (req, res) {
+
+    const id = req.params.id;
+
+    User.findOne({_id: id}, function (err, user) {
+
+        if (err) {
+            return err;
+            console.log('aaaaaaaaaaaa');
+        }
+        res.send(user);
+    });
+
+});
+
+app.put("/api/user/change", jsonParser, function (req, res) {
+
+    if (!req.body) return res.sendStatus(404);
+
+    const id = req.body._id;
+    const newUser = req.body;
+
+    User.findOneAndUpdate({_id: id}, newUser, {new: true}, function (err, user) {
+        if (err) return console.log(err);
+        res.send(user);
+    });
+});
+
+app.put("/api/user/changePassword", jsonParser, function (req, res) {
+
+
+    if (req.body === {}) return res.sendStatus(404);
+    const id = req.body.id;
+    let newUser = {};
+    let b = false;
+
+    newUser.password = req.body.newPassword;
+    newUser.oldPassword = req.body.oldPassword;
+    newUser.salt = req.body.salt;
+
+    User.findOne({_id: id}, function (err, user) {
+
+        if (err) return console.log(err);
+
+
+        if (bcrypt.hashSync(newUser.oldPassword, newUser.salt) === user.password) {
+            console.log("совпадение? не думаю");
+            res.status(200).send('success');
+            b = true;
+        } else {
+            console.log("совпадение это не падение сов");
+            res.status(401).send('Wrong password');
+            b = false;
+        }
+
+    });
+
+
+    if (b) {//комп выполняет эту функцию быстрее т.к. b = false необходимо ввести асинхронные функции
+        let text = bcrypt.hashSync(newUser.password, newUser.salt);
+        console.log(text);
+        console.log("alaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaarm");
+        User.findByIdAndUpdate(id, {password : text}, function (err, user) {
+            if (err) return console.log(err);
+            console.log("I'm here");
+            // user.password = bcrypt.hashSync(newUser.password, newUser.salt);
+            res.status(200).send('success');
+            console.log("Обновленный объект", user);
+        });
+    }else{
+        console.log("nam pizda")
+    }
+
+
 });
--- so.js
+++ so.js
@@ -1,18 +1,44 @@
-// index.js
+// // index.js
+//
+// // подключаем bcrypt
+ var bcrypt = require('bcrypt');
+//
+// // пароль пользователя
+// var passwordFromUser = "test_user_pass";
+//
+// // создаем соль
 
-// подключаем bcrypt
-var bcrypt = require('bcrypt');
+// var saltq = bcrypt.genSaltSync(10);
+// // шифруем пароль
+// var passwordToSave = bcrypt.hashSync(passwordFromUser, salt);
+//
+// // выводим результат
+// console.log(salt);
+// console.log(saltq);
+// console.log(passwordFromUser);
+// console.log(passwordToSave);
+//
+// if (bcrypt.hashSync(passwordFromUser, saltq) === passwordToSave) {
+//     console.log("It's working")
+// }else {
+//     console.log("mistake")
+// }
+//
+//
+//
+// // const getLogin = req.body.userLogin;
+// // const getPassword = req.body.userPassword;
+// // User.findOne({login: getLogin}, function(err, user){
+// //
+// //     if(err) return console.log(err);
+// //
+// //     res.send(user)
+// //
+// // });
+// //
+// // if (bcrypt.hashSync(user.userPassword, user.userId) === getPassword) {
+// //     console.log("It's working")
+// // }else {
+// //     console.log("mistake")
+// // }
 
-// пароль пользователя
-var passwordFromUser = "test_user_pass";
-
-// создаем соль
-var salt = bcrypt.genSaltSync(10);
-
-// шифруем пароль
-var passwordToSave = bcrypt.hashSync(passwordFromUser, salt);
-
-// выводим результат
-console.log(salt);
-console.log(passwordFromUser);
-console.log(passwordToSave);
