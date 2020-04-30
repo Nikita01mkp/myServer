3ae29b040fc273c3581f403e9b63d309801e2882
Nikita01mkp <marcus01hemingway@gmail.com>
2020-04-30 09:29:53 +0300

    Thursday

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -29,14 +29,14 @@ function deleteUser(req, res){
 
     const id = req.query._id;
 
-    User.findOne({_id: id}).populate("users").exec(function (err, user) {
+    User.findOne({_id: id}, function (err, user) {
 
         if (err){
             return res.status(404).send(err);
         }
 
         for(let i = 0; i < user.homes.length; i++){
-            Home.findOne({_id: user.homes[i]._id}).populate("homes").exec(function (err, home) {
+            Home.findOne({_id: user.homes[i]}, function (err, home) {
 
                 if (err) {
                     return res.status(405);
@@ -44,7 +44,7 @@ function deleteUser(req, res){
 
                 for (let i = 0; i < home.rooms.length; i++) {
 
-                    Room.findOne({_id: home.rooms[i]._id}, function (err, room) {
+                    Room.findOne({_id: home.rooms[i]}, function (err, room) {
 
                         if (err) {
                             return res.status(405);
